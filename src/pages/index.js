5851df0af0de9b4598463473176caf6dfffc9d3f
Jordan Cotter <jrdn.cttr@gmail.com>
2020-10-03 12:03:12 -0500

    add title and form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,43 @@
-import React from "react"
+import React, { useState } from 'react'
+import tw, { styled } from 'twin.macro'
+
+const Container = styled.div`
+  ${tw`px-20`}
+`
+const Heading = styled.h1`
+  ${tw`text-center text-3xl`}
+  `
+const Button = styled.button`
+    ${tw`bg-blue-500 hover:bg-blue-800 text-white p-2 rounded`}
+    `
+const Input = styled.input`
+  ${tw`bg-gray-400`}
+`
+const Form = styled.form`
+  ${tw`grid grid-rows-3`}
+`
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const [numChildren, setNumChildren] = useState(0)
+  const [cost, setCost] = useState(0)
+
+  const setNumberOfChildren = evt => {
+    setNumChildren(evt.target.value)
+  }
+  const calculateCost = evt => {
+    setCost(numChildren * 100)
+  }
+
+  return <Container>
+    <Heading>Children of Life Mother's Day Out</Heading> 
+    <Form>
+      <div>Number of Children <Input type="text" onChange={setNumberOfChildren} /></div>
+      <div>Church Member <Input type="checkbox" /></div>
+      <Button onClick={calculateCost}>Calculate Cost</Button>
+    </Form>
+    Cost: { cost }
+  </Container>
+  
 }
+
+
